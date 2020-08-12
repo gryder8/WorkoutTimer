421d013fc6508c039f3db943d3e7880b417721ef
gryder8 <gavin.ryder82@gmail.com>
2020-08-11 17:35:11 -0700

    Initial Commit

---
+++ 10MinWorkout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  10MinWorkout
+//
+//  Created by Gavin Ryder on 8/11/20.
+//  Copyright © 2020 Gavin Ryder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
